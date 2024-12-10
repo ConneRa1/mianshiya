cc9a22090f00a541b2e9cf8187a9b3d3bb182078
ConneRa1 <1158944345@qq.com>
2024-12-10 18:38:22 +0800

    init

---
+++ src/main/java/com/yupi/mianshiya/controller/QuestionBankQuestionController.java
@@ -0,0 +1,165 @@
+package com.yupi.mianshiya.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.mianshiya.annotation.AuthCheck;
+import com.yupi.mianshiya.common.BaseResponse;
+import com.yupi.mianshiya.common.DeleteRequest;
+import com.yupi.mianshiya.common.ErrorCode;
+import com.yupi.mianshiya.common.ResultUtils;
+import com.yupi.mianshiya.constant.UserConstant;
+import com.yupi.mianshiya.exception.BusinessException;
+import com.yupi.mianshiya.exception.ThrowUtils;
+import com.yupi.mianshiya.model.dto.questionBankQuestion.QuestionBankQuestionAddRequest;
+import com.yupi.mianshiya.model.dto.questionBankQuestion.QuestionBankQuestionEditRequest;
+import com.yupi.mianshiya.model.dto.questionBankQuestion.QuestionBankQuestionQueryRequest;
+import com.yupi.mianshiya.model.dto.questionBankQuestion.QuestionBankQuestionUpdateRequest;
+import com.yupi.mianshiya.model.entity.QuestionBankQuestion;
+import com.yupi.mianshiya.model.entity.User;
+import com.yupi.mianshiya.model.vo.QuestionBankQuestionVO;
+import com.yupi.mianshiya.service.QuestionBankQuestionService;
+import com.yupi.mianshiya.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 题库题目接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://www.code-nav.cn">编程导航学习圈</a>
+ */
+@RestController
+@RequestMapping("/questionBankQuestion")
+@Slf4j
+public class QuestionBankQuestionController {
+
+    @Resource
+    private QuestionBankQuestionService questionBankQuestionService;
+
+    @Resource
+    private UserService userService;
+
+    // region 增删改查
+
+    /**
+     * 创建题库题目
+     *
+     * @param questionBankQuestionAddRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/add")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Long> addQuestionBankQuestion(@RequestBody QuestionBankQuestionAddRequest questionBankQuestionAddRequest, HttpServletRequest request) {
+        ThrowUtils.throwIf(questionBankQuestionAddRequest == null, ErrorCode.PARAMS_ERROR);
+        QuestionBankQuestion questionBankQuestion = new QuestionBankQuestion();
+        BeanUtils.copyProperties(questionBankQuestionAddRequest, questionBankQuestion);
+        // 数据校验
+        questionBankQuestionService.validQuestionBankQuestion(questionBankQuestionAddRequest , true);
+        User loginUser = userService.getLoginUser(request);
+        questionBankQuestion.setUserId(loginUser.getId());
+        // 写入数据库
+        boolean result = questionBankQuestionService.save(questionBankQuestion);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        // 返回新写入的数据 id
+        long newQuestionBankQuestionId = questionBankQuestion.getId();
+        return ResultUtils.success(newQuestionBankQuestionId);
+    }
+
+    /**
+     * 删除题库题目
+     *
+     * @param deleteRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/delete")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Boolean> deleteQuestionBankQuestion(@RequestBody DeleteRequest deleteRequest, HttpServletRequest request) {
+        if (deleteRequest == null || deleteRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User user = userService.getLoginUser(request);
+        long id = deleteRequest.getId();
+        // 判断是否存在
+        QuestionBankQuestion oldQuestionBankQuestion = questionBankQuestionService.getById(id);
+        ThrowUtils.throwIf(oldQuestionBankQuestion == null, ErrorCode.NOT_FOUND_ERROR);
+        // 仅本人或管理员可删除
+        if (!oldQuestionBankQuestion.getUserId().equals(user.getId()) && !userService.isAdmin(request)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        // 操作数据库
+        boolean result = questionBankQuestionService.removeById(id);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        return ResultUtils.success(true);
+    }
+
+
+    /**
+     * 分页获取题库题目列表（仅管理员可用）
+     *
+     * @param questionBankQuestionQueryRequest
+     * @return
+     */
+    @PostMapping("/list/page")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Page<QuestionBankQuestion>> listQuestionBankQuestionByPage(@RequestBody QuestionBankQuestionQueryRequest questionBankQuestionQueryRequest) {
+        long current = questionBankQuestionQueryRequest.getCurrent();
+        long size = questionBankQuestionQueryRequest.getPageSize();
+        // 查询数据库
+        Page<QuestionBankQuestion> questionBankQuestionPage = questionBankQuestionService.page(new Page<>(current, size),
+                questionBankQuestionService.getQueryWrapper(questionBankQuestionQueryRequest));
+        return ResultUtils.success(questionBankQuestionPage);
+    }
+
+    /**
+     * 分页获取题库题目列表（封装类）
+     *
+     * @param questionBankQuestionQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/list/page/vo")
+    public BaseResponse<Page<QuestionBankQuestionVO>> listQuestionBankQuestionVOByPage(@RequestBody QuestionBankQuestionQueryRequest questionBankQuestionQueryRequest,
+                                                               HttpServletRequest request) {
+        long current = questionBankQuestionQueryRequest.getCurrent();
+        long size = questionBankQuestionQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        // 查询数据库
+        Page<QuestionBankQuestion> questionBankQuestionPage = questionBankQuestionService.page(new Page<>(current, size),
+                questionBankQuestionService.getQueryWrapper(questionBankQuestionQueryRequest));
+        // 获取封装类
+        return ResultUtils.success(questionBankQuestionService.getQuestionBankQuestionVOPage(questionBankQuestionPage, request));
+    }
+
+    /**
+     * 分页获取当前登录用户创建的题库题目列表
+     *
+     * @param questionBankQuestionQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/my/list/page/vo")
+    public BaseResponse<Page<QuestionBankQuestionVO>> listMyQuestionBankQuestionVOByPage(@RequestBody QuestionBankQuestionQueryRequest questionBankQuestionQueryRequest,
+                                                                 HttpServletRequest request) {
+        ThrowUtils.throwIf(questionBankQuestionQueryRequest == null, ErrorCode.PARAMS_ERROR);
+        // 补充查询条件，只查询当前登录用户的数据
+        User loginUser = userService.getLoginUser(request);
+        questionBankQuestionQueryRequest.setUserId(loginUser.getId());
+        long current = questionBankQuestionQueryRequest.getCurrent();
+        long size = questionBankQuestionQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        // 查询数据库
+        Page<QuestionBankQuestion> questionBankQuestionPage = questionBankQuestionService.page(new Page<>(current, size),
+                questionBankQuestionService.getQueryWrapper(questionBankQuestionQueryRequest));
+        // 获取封装类
+        return ResultUtils.success(questionBankQuestionService.getQuestionBankQuestionVOPage(questionBankQuestionPage, request));
+    }
+
+    // endregion
+}
---
+++ src/main/java/com/yupi/mianshiya/service/impl/QuestionBankQuestionServiceImpl.java
@@ -0,0 +1,146 @@
+package com.yupi.mianshiya.service.impl;
+
+import cn.hutool.core.collection.CollUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yupi.mianshiya.common.ErrorCode;
+import com.yupi.mianshiya.constant.CommonConstant;
+import com.yupi.mianshiya.exception.ThrowUtils;
+import com.yupi.mianshiya.mapper.QuestionBankQuestionMapper;
+import com.yupi.mianshiya.model.dto.questionBankQuestion.QuestionBankQuestionAddRequest;
+import com.yupi.mianshiya.model.dto.questionBankQuestion.QuestionBankQuestionQueryRequest;
+import com.yupi.mianshiya.model.entity.QuestionBankQuestion;
+import com.yupi.mianshiya.model.entity.User;
+import com.yupi.mianshiya.model.vo.QuestionBankQuestionVO;
+import com.yupi.mianshiya.model.vo.UserVO;
+import com.yupi.mianshiya.service.QuestionBankQuestionService;
+import com.yupi.mianshiya.service.UserService;
+import com.yupi.mianshiya.utils.SqlUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 题库题目服务实现
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://www.code-nav.cn">编程导航学习圈</a>
+ */
+@Service
+@Slf4j
+public class QuestionBankQuestionServiceImpl extends ServiceImpl<QuestionBankQuestionMapper, QuestionBankQuestion> implements QuestionBankQuestionService {
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 校验数据
+     *
+     * @param questionBankQuestion
+     * @param add      对创建的数据进行校验
+     */
+    @Override
+    public void validQuestionBankQuestion(QuestionBankQuestionAddRequest questionBankQuestion, boolean add) {
+        ThrowUtils.throwIf(questionBankQuestion == null, ErrorCode.PARAMS_ERROR);
+        // 创建数据时，参数不能为空
+        if (add) {
+            // todo 补充校验规则
+            ThrowUtils.throwIf(questionBankQuestion.getQuestionBankId()==null, ErrorCode.PARAMS_ERROR);
+            ThrowUtils.throwIf(questionBankQuestion.getQuestionId()==null, ErrorCode.PARAMS_ERROR);
+        }
+    }
+
+    /**
+     * 获取查询条件
+     *
+     * @param questionBankQuestionQueryRequest
+     * @return
+     */
+    @Override
+    public QueryWrapper<QuestionBankQuestion> getQueryWrapper(QuestionBankQuestionQueryRequest questionBankQuestionQueryRequest) {
+        QueryWrapper<QuestionBankQuestion> queryWrapper = new QueryWrapper<>();
+        if (questionBankQuestionQueryRequest == null) {
+            return queryWrapper;
+        }
+
+        String sortField = questionBankQuestionQueryRequest.getSortField();
+        String sortOrder = questionBankQuestionQueryRequest.getSortOrder();
+        Long questionBankId = questionBankQuestionQueryRequest.getQuestionBankId();
+        Long questionId = questionBankQuestionQueryRequest.getQuestionId();
+        Long userId = questionBankQuestionQueryRequest.getUserId();
+        // 精确查询
+        queryWrapper.eq(ObjectUtils.isNotEmpty(questionBankId), "questionBankId", questionBankId);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(questionId), "questionId", questionId);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(userId), "userId", userId);
+
+        // 排序规则
+        queryWrapper.orderBy(SqlUtils.validSortField(sortField),
+                sortOrder.equals(CommonConstant.SORT_ORDER_ASC),
+                sortField);
+        return queryWrapper;
+    }
+
+    /**
+     * 获取题库题目封装
+     *
+     * @param questionBankQuestion
+     * @param request
+     * @return
+     */
+    @Override
+    public QuestionBankQuestionVO getQuestionBankQuestionVO(QuestionBankQuestion questionBankQuestion, HttpServletRequest request) {
+        // 对象转封装类
+        QuestionBankQuestionVO questionBankQuestionVO = QuestionBankQuestionVO.objToVo(questionBankQuestion);
+
+        // todo 可以根据需要为封装对象补充值，不需要的内容可以删除
+        // region 可选
+        // 1. 关联查询用户信息
+        Long userId = questionBankQuestion.getUserId();
+        User user = null;
+        if (userId != null && userId > 0) {
+            user = userService.getById(userId);
+        }
+        UserVO userVO = userService.getUserVO(user);
+        questionBankQuestionVO.setUser(userVO);
+        // 2. 已登录，获取用户点赞、收藏状态
+        // endregion
+
+        return questionBankQuestionVO;
+    }
+
+    /**
+     * 分页获取题库题目封装
+     *
+     * @param questionBankQuestionPage
+     * @param request
+     * @return
+     */
+    @Override
+    public Page<QuestionBankQuestionVO> getQuestionBankQuestionVOPage(Page<QuestionBankQuestion> questionBankQuestionPage, HttpServletRequest request) {
+        List<QuestionBankQuestion> questionBankQuestionList = questionBankQuestionPage.getRecords();
+        Page<QuestionBankQuestionVO> questionBankQuestionVOPage = new Page<>(questionBankQuestionPage.getCurrent(), questionBankQuestionPage.getSize(), questionBankQuestionPage.getTotal());
+        if (CollUtil.isEmpty(questionBankQuestionList)) {
+            return questionBankQuestionVOPage;
+        }
+        // 对象列表 => 封装对象列表
+        List<QuestionBankQuestionVO> questionBankQuestionVOList = questionBankQuestionList.stream().map(questionBankQuestion -> {
+            return QuestionBankQuestionVO.objToVo(questionBankQuestion);
+        }).collect(Collectors.toList());
+
+        // todo 可以根据需要为封装对象补充值，不需要的内容可以删除
+        // region 可选
+        // 1. 关联查询用户信息
+        // endregion
+
+        questionBankQuestionVOPage.setRecords(questionBankQuestionVOList);
+        return questionBankQuestionVOPage;
+    }
+
+}
